cf3fa093e4ec1aa7d30af0a73c9be39a18d94259
ace.brn.ali <alipustainacebrian00433@gmail.com>
2026-09-07 00:57:23 +0800

    Initial Commit

---
+++ src/test/java/com/ace_brn_alipustain/jjlc_api/JjlcApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ace_brn_alipustain.jjlc_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JjlcApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
